4d6be11b6435ed4638f3857a7df97b50387151bd
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2018-10-04 21:37:26 +0900

    UITableViewのExtension追加

---
+++ UICatalog/UICatalog/Util/Extension/UITableView+CustomExtension.swift
@@ -0,0 +1,24 @@
+//
+//  UITableView+CustomExtension.swift
+//  UICatalog
+//
+//  Created by Takuya Yokoyama on 2018/10/04.
+//  Copyright © 2018 Takuya Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    func scrollToTop(animated: Bool) {
+        setContentOffset(CGPoint(x: 0, y: -contentInset.top), animated: animated)
+    }
+    
+    func scrollToBottom(animated: Bool) {
+        let contentOffset = CGPoint(
+            x: 0.0,
+            y: contentSize.height - frame.size.height + contentInset.bottom
+        )
+        setContentOffset(contentOffset, animated: animated)
+    }
+}
+
